b63ee803ab36f60255e01bf912ada1ba4b223752
Chris Araman <chris-araman@users.noreply.github.com>
2021-05-10 14:10:47 -0700

    ♻️ Move URL builders to MasStoreSearch

--- Sources/MasKit/Controllers/StoreSearch.swift
+++ Sources/MasKit/Controllers/StoreSearch.swift
@@ -6,44 +6,10 @@
 //  Copyright © 2018 mas-cli. All rights reserved.
 //
 
-import Foundation
 import PromiseKit
 
 /// Protocol for searching the MAS catalog.
 protocol StoreSearch {
     func lookup(app appId: Int) -> Promise<SearchResult?>
     func search(for appName: String) -> Promise<[SearchResult]>
-}
-
-// MARK: - Common methods
-extension StoreSearch {
-    /// Builds the search URL for an app.
-    ///
-    /// - Parameter appName: MAS app identifier.
-    /// - Returns: URL for the search service or nil if appName can't be encoded.
-    func searchURL(for appName: String) -> URL? {
-        guard var components = URLComponents(string: "https://itunes.apple.com/search") else {
-            return nil
-        }
-
-        components.queryItems = [
-            URLQueryItem(name: "media", value: "software"),
-            URLQueryItem(name: "entity", value: "macSoftware"),
-            URLQueryItem(name: "term", value: appName),
-        ]
-        return components.url
-    }
-
-    /// Builds the lookup URL for an app.
-    ///
-    /// - Parameter appId: MAS app identifier.
-    /// - Returns: URL for the lookup service or nil if appId can't be encoded.
-    func lookupURL(forApp appId: Int) -> URL? {
-        guard var components = URLComponents(string: "https://itunes.apple.com/lookup") else {
-            return nil
-        }
-
-        components.queryItems = [URLQueryItem(name: "id", value: "\(appId)")]
-        return components.url
-    }
 }
